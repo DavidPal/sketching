95918a6a1ebe4fda5d21930d6f4ff1de34124bec
David Pal <davidko.pal@gmail.com>
2018-05-10 20:18:46 -0400

    First commit

---
+++ src/test/java/com/palenica/david/AppTest.java
@@ -0,0 +1,15 @@
+package com.palenica.david;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class AppTest {
+
+    @Test
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+
+}
